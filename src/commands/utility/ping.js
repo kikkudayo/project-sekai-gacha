5c03b7f4ba1f1689b7a0ae68382caecfed386360
unknown <gabisaac2000@gmail.com>
2024-06-24 19:06:04 +0800

    added commands folder

---
+++ src/commands/utility/ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("wonderhoy")
+    .setDescription("Wonderhoy!"),
+  async execute(interaction) {
+    await interaction.reply("Pong!");
+  },
+};
